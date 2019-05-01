41242f937513cd9a9a11527f6dbdcf6d01f75a9d
moin <moin2672@gmail.com>
2019-05-01 16:53:47 +0530

    docheck

--- src/app/lchild2/lchild2.component.ts
+++ src/app/lchild2/lchild2.component.ts
@@ -1,20 +1,34 @@
-import { Component, Input, OnInit, OnChanges, SimpleChanges, SimpleChange,ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, DoCheck, SimpleChanges, SimpleChange,ChangeDetectionStrategy } from '@angular/core';
 import {Customer1} from '../customer1';
 @Component({
   selector: 'lchild2',
   templateUrl: './lchild2.component.html',
   styleUrls: ['./lchild2.component.css']
 })
-export class Lchild2Component implements OnChanges, OnInit {
+export class Lchild2Component implements OnChanges, DoCheck, OnInit {
 
   @Input() message: string;
   @Input() customer: Customer1;
   changelog: string[] = [];
-
+  oldCustomer: Customer1= new Customer1();
+  DocheckCount = 0;
+  differ: any;
+  
   ngOnInit() {
       console.log('OnInit');
   }
+  ngDoCheck() {
+    console.log('Docheck');
+    this.DocheckCount++;
+    if (this.oldCustomer.name !== this.customer.name || this.oldCustomer.code !== this.customer.code ) {
+        const to  = JSON.stringify(this.customer);
+        const from = JSON.stringify(this.oldCustomer);
+        const changeLog = `DoCheck customer: changed from ${from} to ${to} `;
+        this.changelog.push(changeLog);
 
+        this.oldCustomer = Object.assign({}, this.customer);
+    }
+}
   ngOnChanges(changes: SimpleChanges) {
       console.log('OnChanges');
       console.log(JSON.stringify(changes));
